56ba11e3c1ff4b80e4f9d5c0262836c0066f151b
SHANIZZLE <shannenreese@gmail.com>
2018-03-18 17:48:10 -0500

    Im an idiot

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ let app = express();
 
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
+// const port = 3000;
 
 //Use post http method to create new resources(todo) and you send that resource as a body
 
