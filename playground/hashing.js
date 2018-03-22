bce20784d3bc128784b246f966daf6314989e495
SHANIZZLE <shannenreese@gmail.com>
2018-03-22 15:53:47 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = 'abc123!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+let hashedPassword = '$2a$10$.kc/fw9Xpas/XwbXn9nBK.aLtFdpzzxWKCUM9rQOtP3RFc5Pe/HRy';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
 
 
-let token = jwt.sign(data, '123abc');
-  console.log(token);
-let decoded = jwt.verify(token, '123abc');
-  console.log('decoded', decoded);
+
+
+// let data = {
+//   id: 10
+// };
+//
+//
+//
+// let token = jwt.sign(data, '123abc');
+//   console.log(token);
+// let decoded = jwt.verify(token, '123abc');
+//   console.log('decoded', decoded);
 // let message = 'I am user number 3';
 // let hash =  SHA256(message).toString();
 //
