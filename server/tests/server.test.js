c56afab470020b641204d7f562f870d225a1dadb
SHANIZZLE <shannenreese@gmail.com>
2018-03-24 12:52:44 -0500

    Updated to expect 21.1.0 and updated test files

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -125,7 +125,7 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
        }
 
        Todo.findById(hexId).then((todo) => {
-         expect(todo).toNotExist();
+         expect(todo).toBeFalsy();
          done();
        }).catch((e) => done(e));
      });
@@ -144,7 +144,7 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
        }
 
        Todo.findById(hexId).then((todo) => {
-         expect(todo).toExist();
+         expect(todo).toBeTruthy();
          done();
        }).catch((e) => done(e));
      });
@@ -186,7 +186,7 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
         .expect((res) => {
           expect(res.body.todo.text).toBe(text);
           expect(res.body.todo.completed).toBe(true);
-          expect(res.body.todo.completedAt).toBeA('number');
+          expect(typeof res.body.todo.completedAt).toBe('number');
         })
         .end(done);
     });
@@ -259,8 +259,8 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
         .send({email, password})
         .expect(200)
         .expect((res) => {
-          expect(res.header['x-auth']).toExist();
-          expect(res.body._id).toExist();
+          expect(res.header['x-auth']).toBeTruthy();
+          expect(res.body._id).toBeTruthy();
           expect(res.body.email).toBe(email);
         })
         .end((err) => {
@@ -269,8 +269,8 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
           }
 
           User.findOne({email}).then((user) => {
-            expect(user).toExist();
-            expect(user.password).toNotBe(password);
+            expect(user).toBeTruthy();
+            expect(user.password).not.toBe(password);
             done();
           }).catch((e) => done(e));
         });
@@ -309,7 +309,7 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
       })
       .expect(200)
       .expect((res) => {
-        expect(res.headers['x-auth']).toExist();
+        expect(res.headers['x-auth']).toBeTruthy();
       })
       .end((err, res) => {
         if (err) {
@@ -317,7 +317,7 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[1]).toInclude({
+          expect(user.toObject().tokens[1]).toMatchObject({
             access: 'auth',
             token: res.headers['x-auth']
           });
@@ -335,7 +335,7 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
       })
       .expect(400)
       .expect((res) => {
-        expect(res.headers['x-auth']).toNotExist();
+        expect(res.headers['x-auth']).toBeFalsy();
       })
       .end((err, res) => {
         if (err) {
